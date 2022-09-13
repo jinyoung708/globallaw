32dab8d6662199c03322ad25f0dc8e93b315797d
DESKTOP-URR6JPI\jinya <jinyang708@gmail.com>
2022-09-13 22:51:19 +0900

    2022-09-13 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,23 @@
 $(function(){
-    
+    $(".m_gnb_btn").on("click", function(){
+        $("body").css({"overflow": "hidden"});
+        $("#header .m_gnb_box").addClass("show");
+    });
+
+    $(".m_gnb_close").on("click", function(){
+        $("body").css({"overflow": "visible"});
+        $("#header .m_gnb_box").removeClass("show");
+    });
+
+    $(window).on("scroll", function(){
+        pageY = window.pageYOffset;
+        if(pageY>$("#content").offset().top){
+            $("#header").addClass("scrolled");
+        }
+        else{
+            $("#header").removeClass("scrolled");
+        }
+        
+    });
+    $(window).scroll();
 });
